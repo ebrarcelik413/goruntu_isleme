c600ccb7378fce8f354d2b6b8dfb00c102a1c449
ebrarcelik413 <ebrarcelikk413@gmail.com>
2025-09-28 18:43:25 +0300

    V01.01.01
    Add color to gray function files

---
+++ color2gray/rgb.py
@@ -0,0 +1,20 @@
+import cv2
+
+# Fotoğrafın tam yolu
+img_path = r"C:\Users\Ebrar Celik\Desktop\goruntu_renklendirme\siyah_beyaz.png"
+
+# Siyah-beyaz fotoğrafı oku
+gray = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+if gray is None:
+    print("⚠️ Fotoğraf bulunamadı! Dosya yolunu kontrol et.")
+else:
+    print("✅ Fotoğraf yüklendi, boyut:", gray.shape)
+    # Colormap uygula
+    color_img = cv2.applyColorMap(gray, cv2.COLORMAP_JET)
+
+    # Sonucu göster
+    cv2.imshow("Renklendirilmis", color_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
