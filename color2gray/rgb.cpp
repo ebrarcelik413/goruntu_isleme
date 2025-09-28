4c76d1d2d75fe06e60972773680e7b802eae9b5d
ebrarcelik413 <ebrarcelikk413@gmail.com>
2025-09-28 18:58:19 +0300

    V01.01.03 Entegration to vs code

---
+++ color2gray/rgb.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Giriş/çıkış kütüphanesi
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // Ekrana yazdırır
+    return 0; // Programın başarıyla bittiğini bildirir
+}
