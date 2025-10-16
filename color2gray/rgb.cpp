07c55a05b2e86a6a33022d23ebb8fdcb0d311d6e
ebrarcelik413 <ebrarcelikk413@gmail.com>
2025-10-16 16:04:28 +0300

    Update rgb.cpp

--- color2gray/rgb.cpp
+++ color2gray/rgb.cpp
@@ -1,6 +1,29 @@
-#include <iostream> // Giriş/çıkış kütüphanesi
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl; // Ekrana yazdırır
-    return 0; // Programın başarıyla bittiğini bildirir
+    string path = "test.png";
+    cout << "Dosya yolu: " << path << endl;
+
+    Mat image = imread(path);
+    if (image.empty()) {
+        cout << "Resim yüklenemedi! Lütfen dosya yolunu kontrol et." << endl;
+        system("dir"); 
+        return -1;
+    }
+
+    cout << "Orijinal resim boyutu: " << image.cols << "x" << image.rows << endl;
+
+    // ---- Renkli resmi griye dönüştür ----
+    Mat grayImage;
+    cvtColor(image, grayImage, COLOR_BGR2GRAY);
+
+    // ---- Sonuçları göster ----
+    imshow("Orijinal", image);
+    imshow("Siyah Beyaz", grayImage);
+
+    waitKey(0);
+    return 0;
 }
